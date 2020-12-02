98c4e72c1c62b09125385ba885efe8d10c3f8aed
VikaKost <viktoria16.kos@mail.ru>
2020-12-02 16:50:55 +0300

    fixes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const messages = [
   {
     id: 'm1',
@@ -186,13 +188,42 @@ const messages = [
 ];
 
 const users = ['Max', 'Alina', 'Vlad', 'Liza', 'Dasha', 'Катя', 'Nik', 'Вика', 'Sasha', 'Vlad', 'Olha'];
-const activeUsers = ['Max', 'Alina', 'Vlad', 'Liza', 'Dasha', 'Катя', 'Nik', 'Вика', 'Sasha'];
+const activeUsers = ['Max', 'Alina', 'Vlad', 'Liza', 'Dasha', 'Катя', 'Sasha'];
 
 class UserList{
  constructor(users, activeUsers){
-    this.users = users;
+   const userList = this.restoreUsers();
+    this.users = userList;
     this.activeUsers = activeUsers;
+
   }
+  addUser(user){
+    this.saveUsers(user)
+   users.push(user);
+    messageList._user = user;
+  }
+  saveUsers(user){
+    localStorage.setItem('user',user);
+    localStorage.setItem('user_' + this.users.length+1,user);
+  }
+
+  restoreUsers(){
+    let usersArray = [];
+    let value;
+    let j = 0;
+    for(let i = 0; i<localStorage.length; i++){
+      let tmp = localStorage.key(i);
+      if (tmp.includes('user_')){
+        value = localStorage.getItem(tmp);
+       // value = JSON.parse(value);
+        usersArray[j] = value;
+        j++;
+      }
+    }
+    console.log(usersArray);
+    return usersArray;
+  }
+
 }
 class HeaderView{
   constructor(containerId) {
@@ -214,6 +245,20 @@ class HeaderView{
 }
 }
 
+class UsersSelect{
+  constructor(containerId) {
+    this.iduser = containerId
+  }
+  display(users){
+    let select = document.getElementById(this.iduser);
+    let usersList = '<option>Всем</option>'
+    users.forEach(item =>{
+      usersList += `<option>${item}</option>`
+    })
+    select.innerHTML = usersList;
+  }
+}
+
 class MessagesView{
   constructor(containerId) {
     this.idmes = containerId
@@ -222,17 +267,17 @@ class MessagesView{
     let msgs = document.getElementById(this.idmes);
     let mesList = '<div id="msgs-list">';
     msg.forEach(item =>{
-      let time = item.createdAt.getHours()+':'+item.createdAt.getMinutes();
+      let time = item.createdAt.getDay() + '.' + item.createdAt.getMonth()+ '.' +item.createdAt.getFullYear() +'/'+ item.createdAt.getHours()+':'+item.createdAt.getMinutes();
       let author = item.author;
 
-      if (item.to && item.author !== currentUser){
+      if (item.isPersonal && item.to === messageList.user){
           mesList += `<div class='mes private-mes' id= ${item.id}>
                       <img class='user-img' src='images/mn_icon.png'>`
       }
-      else if(item.author === currentUser){
-        mesList += `<div class='mes self-mes' > <div class="self-edit" id= ${item.id}>
-       <span onclick="ToEdit(this)"><span title="Изменить" class="iconify ed click" data-icon="emojione-monotone:pencil" data-inline="false"></span></span>
-        <span onclick="ToDelete(this)" title="Удалить" class="iconify del ed click" data-icon="carbon:delete" data-inline="false"></span>
+      else if(item.author === MessageList._user){
+        mesList += `<div class='mes self-mes' id= ${item.id}> <div class="self-edit" >
+       <div  class="toEdit"><span title="Изменить" class="iconify ed click" data-icon="emojione-monotone:pencil" data-inline="false"></span></div>
+       <div class="toDelete"> <span  title="Удалить" class="iconify del ed click" data-icon="carbon:delete" data-inline="false"></span></div>
       </div>`
         author = 'Вы';
         if (item.to){
@@ -316,22 +361,53 @@ class Message {
 }
 
  class MessageList{
-   constructor(user, msgs) {
-     this._user = user;
+   constructor() {
+     this._user = localStorage.getItem('user') || undefined;
+     const msgs = this.restore();
      this._messages = [];
      msgs.forEach(item => {
        this._messages.push(new Message(item));
      });
    }
 
-   get user(){
-     return this._user;
-   };
+   get messages(){
+     return this._messages;
+   }
+
    set user(value){
      throw new Error("this field can't be changed");
    };
 
+   save(mes){
+     let msg = {}
+     msg.id = mes._id;
+     msg.text = mes.text;
+     msg.createdAt = mes._createdAt;
+     msg.author = mes._author;
+     msg.isPersonal = mes.isPersonal;
+     if (mes.to){
+       msg.to = mes.to;
+     }
+     let json = JSON.stringify(msg);
+     localStorage.setItem(msg.id,json);
+   }
 
+   restore(){
+     let msgs = [];
+     let value;
+     let j = 0;
+     for(let i = 0; i<localStorage.length; i++){
+       let tmp = localStorage.key(i);
+        if (tmp.includes('m')){
+         value = localStorage.getItem(tmp);
+         value = JSON.parse(value);
+         value.createdAt = new Date (value.createdAt);
+         msgs[j] = value;
+         j++;
+       }
+     }
+     return msgs;
+   }
      getPage( skip = 0, top = 10, filterConfig={}) {
 
        const filterObj = {
@@ -342,7 +418,7 @@ class Message {
        };
 
      let result = this._messages.slice();
-       result = this._messages.filter(item=> !item.to || item.to === this.user || item.author === this.user);
+       result = this._messages.filter(item=> !item.to || item.to === this._user || item.author === this._user);
      Object.keys(filterConfig).forEach((key) => {
        result = result.filter((item) => filterObj[key](item, filterConfig[key]));
      });
@@ -368,9 +444,11 @@ class Message {
    }
 
     add(msg){
-       const mes = new Message({id : 'm' + String(+new Date()), createdAt : new Date(), text: msg.text, author: this.user,  to: msg.to});
+       const mes = new Message({id : 'm' + String(+new Date()), createdAt : new Date(), text: msg.text, author: this._user,  to: msg.to});
        if (this.validate(mes)){
+         this.save(mes);
        this._messages.push(mes);
+
        return true;
      }
      else {
@@ -380,14 +458,14 @@ class Message {
 
    edit(id, msg){
      let idEdit = this.get(id);
-
-     if ((this.user !== idEdit.author)){
+     if ((this._user !== idEdit.author)){
        return false;
      }
      if (this.validate(idEdit) ){
        for(let key in msg){
          idEdit[key] = msg[key];
        }
+       this.save(idEdit);
        return true;
      }
      else{
@@ -397,10 +475,11 @@ class Message {
 
    remove(id){
      let idRemove = this._messages.findIndex( currentValue => currentValue.id === id );
-     if (this.user !== this._messages[idRemove].author){
+     if (this._user !== this._messages[idRemove].author){
        return false;
      }
      this._messages.splice(idRemove,1);
+     localStorage.removeItem(id);
      return true;
 
    }
@@ -424,47 +503,6 @@ class Message {
 
 }
 
-function showMessages(skip, top, filterConfig){
-  let messagesList = list.getPage(skip, top, filterConfig);
-    messagesView.display(messagesList);
-}
-
-function showActiveUsers(){
-  activeUsersView.display(userList.activeUsers);
-}
-
-function setCurrentUser(user){
-
-  headerView.display(user);
-  return user;
-}
-
-function addMessage(msg){
-  if (list.add(msg)){
-    showMessages(0, 10);
-    return true;
-  }
-  return false;
-}
-
-function editMessage(id,msg){
-  if (list.edit(id, msg)) {
-    showMessages(0, 10);
-    return true;
-  }
-  return false;
-}
-
-function removeMessage(id){
-  if (list.remove(id)) {
-   showMessages(0, 10);
-   return true
-   }
-  return false
-}
-
-
-
 function ChangeForm(blockFrom, blockTo){
   let from = document.getElementById(blockFrom);
   let to = document.getElementById(blockTo);
@@ -473,64 +511,214 @@ function ChangeForm(blockFrom, blockTo){
    to.style.display = "block";
 }
 
-function SendMessage(){
 
-   let input = document.getElementById('addMes');
-  if(input.value){
-     let text = document.getElementById('text');
-    editMessage(input.value, {text: text.value});
-    text.blur();
-    return;
-   }
-  let text = document.getElementById('text');
-  addMessage({text: text.value});
-  text.blur();
+class Controller {
+  constructor() {
+    this.userList = new UserList(users, activeUsers);
+    this.headerView = new HeaderView('user');
+    this.messagesView = new MessagesView('list');
+    this.activeUsersView = new ActiveUsersView('usersList');
+    this.usersSelect = new UsersSelect('users');
 
-
-}
-
-function ToEdit(edit){
-  let id = edit.parentNode.id;
-  let msg = list.get(id);
-  document.getElementById('addMes').value = id;
-  document.getElementById('text').value = msg.text;
-}
-
-function ToDelete(del){
-  if(confirm('Удалить сообщение?')) {
-    let id = del.parentNode.id;
-    removeMessage(id)
   }
+
+
+
+  setCurrentUser(user) {
+    if (user){
+      MessageList._user = user;
+      this.headerView.display(user);
+    }
+    else{
+      MessageList._user = localStorage.getItem('user') || undefined
+      this.headerView.display(MessageList._user);
+    }
+  }
+
+  showActiveUsers() {
+    this.activeUsersView.display(this.userList.activeUsers);
+  }
+
+  showSelectUsers(){
+    this.usersSelect.display(this.userList.activeUsers);
+  }
+
+  showMessages(skip, top, filterConfig) {
+    let messagesList = messageList.getPage(skip, top, filterConfig);
+    this.messagesView.display(messagesList);
+  }
+
+  addMessage(msg) {
+    if (messageList.add(msg)) {
+      this.showMessages(0, mesAmount);
+      return true;
+    }
+    return false;
+  }
+
+  editMessage(id, msg) {
+    if (messageList.edit(id, msg)) {
+      this.showMessages(0, mesAmount);
+      return true;
+    }
+    return false;
+  }
+
+  removeMessage(id) {
+    if (messageList.remove(id)) {
+      this.showMessages(0, mesAmount);
+      return true
+    }
+    return false
+  }
+
+  ToSend(event) {
+      let msg = {}
+      if (select.value !== 'Всем'){
+        msg.to = select.value;
+      }
+
+    let text = event.srcElement[0].value;
+    //console.log(event.currentTarget.parentNode.value);
+    if (sendBtn.value === ''){
+      msg.text = text;
+      this.addMessage(msg);
+    }
+    else{
+      msg.text = text
+      this.editMessage(sendBtn.value,msg)
+      sendBtn.value = '';
+    }
+  }
+
+  ToEdit(msg){
+    sendBtn.value = msg.id;
+    text.value = msg.text;
+    text.focus();
+  }
+
+  ToDelete(del){
+    if(confirm('Удалить сообщение?')) {
+      controller.removeMessage(del)
+    }
+  }
+
+  ChangeMessage(){
+    let target = event.target;
+    let id = target.parentNode.parentNode.parentNode.id
+    let msg = messageList.get(id);
+    if(target.parentNode.className === 'toEdit'){
+      controller.ToEdit(msg)
+    }
+    else if(target.parentNode.className === 'toDelete'){
+      controller.ToDelete(msg.id);
+    }
+
+  }
+
+
+ToFilter(event){
+  let filter = {};
+    if(event.target[0].value){
+      filter.author = event.target[0].value;
+    }
+    if(event.target[1].value){
+      filter.text = event.target[1].value;
+    }
+    if(event.target[2].value){
+      filter.dateFrom = new Date (event.target[2].value);
+    }
+    if(event.target[3].value){
+      filter.dateTo = new Date (event.target[3].value)
+    }
+  this.showMessages(0, 10, filter);
 }
 
 
+  Registration(event) {
+    if (event.target[1].value === event.target[2].value && event.target[1].value && event.target[2].value){
+      this.setCurrentUser(event.target[0].value);
+       this.userList.addUser(event.target[0].value);
+       this.showMessages(0, 10);
+      ChangeForm('regForm', 'formMain')
+     }
+    else if(!event.target[0].value){
+      alert('Заполните форму!');
+    }
+    else{
+      alert('Пароли не совпадают');
+    }
+
+  }
+
+  Login(event){
+    if (event.target[0].value && event.target[1].value){
+      messageList._user = event.target[0].value;
+      localStorage.setItem('user',event.target[0].value);
+      this.setCurrentUser(event.target[0].value);
+      this.showMessages(0, 10);
+      ChangeForm('loginForm', 'formMain')
+    }
+    else{
+      alert('Заполните форму!');
+    }
+  }
+
+   ShowMore(){
+   mesAmount += 10;
+    controller.showMessages(0,mesAmount);
+  }
+
+}
+
+function FillStorage(){
+  messages.forEach(item =>{
+    let json = JSON.stringify(item);
+    localStorage.setItem(item.id,json);
+  })
+  let usersCol = 0;
+  users.forEach(item =>{
+
+    localStorage.setItem('user_'+usersCol,item );
+    usersCol++;
+  })
+
+  let onlCol = 0;
+  activeUsers.forEach(item =>{
+
+    localStorage.setItem('onlUser_'+onlCol,item );
+    onlCol++;
+  })
+
+}
+if (localStorage.length === 0){
+  FillStorage();
+}
+
+let mesAmount = 10;
+const messageList = new MessageList();
+const controller = new Controller();
+
+controller.setCurrentUser();
+
+controller.showActiveUsers();
+controller.showMessages(0,10);
+
+controller.showSelectUsers();
+
+const show = document.getElementById('showMore');
+show.addEventListener('click', controller.ShowMore);
+const select = document.getElementById('users');
+const text = document.getElementById('text');
+const sendBtn = document.getElementById('addMes');
+
+const list = document.getElementById("list");
+list.addEventListener('click', controller.ChangeMessage);
 
 
-const headerView = new HeaderView('user');
-const messagesView = new MessagesView('list');
-const activeUsersView = new ActiveUsersView('usersList');
-let currentUser = setCurrentUser('Viktoria');
-const list = new MessageList(currentUser, messages);
-const userList = new UserList(users, activeUsers);
-
-editMessage('m22', {text: 'Текст измененного сообщения'});
-showActiveUsers();
-showMessages(0,10);
-addMessage({text:'Текст нового сообщения', to: 'Liza'});
-
-//removeMessage('22');
 
 
 
-//console.log(list.add({text:'Текст нового сообщения', to: 'Liza'}));
-//console.log(list.remove('4'));
-//console.log(list.getPage(0, 10));
-//console.log(list._messages);
-//console.log(list.edit('5', {text: 'Текст измененного сообщения'}));
-//console.log(users.users);
-//console.log(list.addAll(messages));
-//console.log(list._messages);
-//console.log(list);
-//console.log(list.clear());
-//console.log(list.get('5'));
-//console.log( Object.getOwnPropertyDescriptor (mes, 'id'));
+
+
+
